bb805e5f779b958b936641206544fbd3c36b4ed3
Roger Camps Viader <rcamps-v@cbr5s5.42barcelona.com>
2025-12-04 15:14:05 +0100

    commit1

--- ft_printf.c
+++ ft_printf.c
@@ -3,14 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rcamps-v <rcamps-v@student.42barcelon      +#+  +:+       +#+        */
+/*   By: rcamps-v <rcamps-v@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 15:58:17 by rcamps-v          #+#    #+#             */
-/*   Updated: 2025/12/02 16:19:38 by rcamps-v         ###   ########.fr       */
+/*   Updated: 2025/12/04 14:58:16 by rcamps-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_printf(char const *, ...)
+int	ft_printf(char const *larg, ...)
 {
+	va_list	vl;
+	size_t	i;
+	size_t	j;
+	int		*str;
 
+	va_start(vl, larg);
+	i = 1;
+	j = 1;
+	while (larg[i])
+	{
+		if (larg[i] == 's' && larg[i - 1] == '%')
+		{
+			str[j] = va_arg();
+		}
+	}
 }
